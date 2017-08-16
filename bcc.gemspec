d4ff20d5c752da95ff63cf20be9ea49df23f99eb
rjmintn <rjmorawski@gmail.com>
2017-08-16 00:37:37 -0400

    auth complete

--- bcc.gemspec
+++ bcc.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name = 'bcc'
-  s.version = '0.0.1'
-  s.date = '2017-8-14'
+  s.version = '0.0.2'
+  s.date = '2017-08-14'
   s.summary = 'Bloc Course API Client'
   s.description = 'An API client for Bloc courses.'
   s.authors = ['RJ Morawski']
@@ -10,5 +10,6 @@ Gem::Specification.new do |s|
   s.require_paths = ["lib"]
   s.homepage = 'http://rubygems.org/gems/bcc'
   s.license = 'MIT'
-  s.add_runtime_dependency 'httparty', '~> 0.13'
+  s.add_runtime_dependency 'httparty', '~> 0.15'
+  # s.add_runtime_dependency 'json', '~> 0.15'
 end
---
+++ lib/bcc.rb
@@ -0,0 +1,33 @@
+require 'httparty'
+class Bcc
+  include HTTParty
+  base_uri 'https://www.bloc.io/api/v1'
+
+  def initialize(e, p)
+    values = {body: {
+      email: e,
+      password: p
+    }}
+    response = self.class.post("/sessions", values)
+    puts response['auth_token']
+  end
+
+
+
+end
+
+
+
+
+
+  # require 'httparty'
+  # require 'json'
+  #
+  #     values = {body: {
+  #       "email": "rjmorawski@gmail.com",
+  #       "password": "wd212dr1"
+  #     }}
+  #
+  # response = HTTParty.post('https://www.bloc.io/api/v1/sessions', values)
+  # puts JSON.pretty_generate(response)
+  # puts response
